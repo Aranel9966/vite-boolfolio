dd9e9aec97e323d5609c7660860224a10276f1ca
Aranel9966 <99angelo.dire@gmail.com>
2023-05-29 18:32:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script >
+import AppMain from './components/AppMain.vue'
+export default{
+
+  data() {
+    return {
+      
+    }
+  },
+  components:{
+    AppMain,
+  }
+}
+</script>
+
+<template>
+
+  <AppMain></AppMain>
+</template>
+
+<style >
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,75 @@
+<script>
+
+import axios from 'axios';
+
+import ProjectCard from './ProjectCard.vue';
+
+export default {
+  name: 'AppMain',
+
+  data() {
+    return {
+      projects: [],
+
+      
+    }
+  },
+
+  components: {
+    ProjectCard,
+  },  
+
+  created() {
+    this.getProjects();
+  },
+
+  methods: {
+    getProjects() {
+
+
+
+      axios.get('http://127.0.0.1:8000/api/projects').then(response => {
+        console.log(response.data.results);
+        this.projects = response.data.results;
+      });
+
+    },
+
+  },
+}
+</script>
+
+<template>
+
+  <div class="container pt-5">
+
+
+    <hr>
+
+    <div class="row">
+      <div v-for="project in projects" class="col-4 mb-5">
+
+        <ProjectCard :project="project"></ProjectCard>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- <div v-else class="loading-screen">
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div> -->
+
+  
+</template>
+
+<style lang="scss">
+// .loading-screen {
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   height: 100%;
+//   min-height: 600px;
+// }
+</style>
